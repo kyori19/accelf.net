2eaa6ca9e0ac739274a3485ec646724c86283e60
kyori19 <kyori@accelf.net>
2021-10-27 16:55:39 +0000

    Fix HTML self-closing tag style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,26 +8,26 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={classNames(styles.root, styles.dark)}>
-      <header className={styles.header}>
-        <svg viewBox="0 0 400 204" width={60} height={30}>
-          <use href="/logo.svg#symbol" />
-        </svg>
-      </header>
-      <div className={styles.content}>
-        <Component {...pageProps} />
+      <div className={classNames(styles.root, styles.dark)}>
+        <header className={styles.header}>
+          <svg viewBox="0 0 400 204" width={60} height={30}>
+            <use href="/logo.svg#symbol" />
+          </svg>
+        </header>
+        <div className={styles.content}>
+          <Component {...pageProps} />
+        </div>
+        <footer className={styles.footer}>
+          <p>
+            加速を与える力のように<span className={styles.space}>-</span><A href="/">Team AccelForce</A>
+          </p>
+          <p>
+            <A className={styles.space} href="https://odakyu.app/@ars42525" rel="me">@ars42525@odakyu.app</A>
+            <A className={styles.space} href="https://github.com/kyori19/accelf.net">Source Code</A>
+            <span className={styles.space}>Hosted on <A href="https://vercel.com">Vercel</A></span>
+          </p>
+        </footer>
       </div>
-      <footer className={styles.footer}>
-        <p>
-          加速を与える力のように<span className={styles.space}>-</span><A href='/'>Team AccelForce</A>
-        </p>
-        <p>
-          <A className={styles.space} href="https://odakyu.app/@ars42525" rel="me">@ars42525@odakyu.app</A>
-          <A className={styles.space} href="https://github.com/kyori19/accelf.net">Source Code</A>
-          <span className={styles.space}>Hosted on <A href="https://vercel.com">Vercel</A></span>
-        </p>
-      </footer>
-    </div>
   );
 }
 
