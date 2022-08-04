020470a6c1151177c01ee81b33d14a7c6481e130
kyori19 <kyori@accelf.net>
2022-08-04 11:19:15 +0900

    Update [[...slug]].tsx

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -23,7 +23,7 @@ export const getStaticPaths: GetStaticPaths<PageUrlProps> = () => pageIndex()
     .then((pages) => ({
       paths: pages.filter(({ published, slug }) => published && slug !== 'blog')
           .map(({ slug }) => ({ params: { slug: slug.split('/') } })),
-      fallback: false,
+      fallback: 'blocking',
     }));
 
 export const getStaticProps: GetStaticProps<PageProps, PageUrlProps> = ({ params }) => pageIndex()
